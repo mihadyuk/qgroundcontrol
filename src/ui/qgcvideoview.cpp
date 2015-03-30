9792f5b5abbde7c82576a80d999c9a6ab8ccfe3a
Anatoliy Stepanenko <tolya_bsuir@mail.ru>
2015-03-30 10:21:14 +0300

    - Using mpv instead mplayer.

--- src/ui/qgcvideoview.cpp
+++ src/ui/qgcvideoview.cpp
@@ -2,18 +2,31 @@
 #include "ui_qgcvideoview.h"
 #include <QDebug>
 
+
 QGCVideoView::QGCVideoView(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::QGCVideoView)
 {
-    QString prog = "mplayer";
-    QStringList args;
-    args << "-wid" <<
-            QString::number((int)(this->winId())) <<
-            "/home/stepanenko/1234.mp4";
+//    QString prog = "mplayer";
+//    QStringList args;
+//    args << "-wid" <<
+//            QString::number((int)(this->winId())) <<
+//            "/home/stepanenko/1234.mp4";
 
     ui->setupUi(this);
-    proc.start(prog ,args);
+    timer = new QTimer(this);
+    timer->setSingleShot(true);
+
+    connect(timer, SIGNAL(timeout()), this, SLOT(elapsed()));
+
+
+    //proc.start(prog ,args);
+
+    timer->start(1000);
+
+
+//    this->repaint();
+//    this->update();
 }
 
 QGCVideoView::~QGCVideoView()
@@ -28,4 +41,24 @@ void QGCVideoView::closeEvent(QCloseEvent *event){
     //this->deleteLater();
     event->ignore();
 
+}
+
+void QGCVideoView::mouseMoveEvent(QMouseEvent *event){
+    event->ignore();
+}
+
+void QGCVideoView::elapsed(){
+//    QString prog = "/home/stepanenko/PROJECTAS/mpv/mpv-build/mpv/build/mpv";
+//    QStringList args;
+//    args << "-wid" <<
+//            QString::number((int)(this->winId())) <<
+//            "/home/stepanenko/1234.mp4";
+
+    QString prog = "/home/stepanenko/PROJECTAS/mpv/mpv-build/mpv/build/mpv";
+    QStringList args;
+    args << "-wid" <<
+            QString::number((int)(this->winId())) <<
+            "udp://127.0.0.1:1300";
+
+    proc.start(prog ,args);
 }
